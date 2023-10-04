8240be063496ecba90647efd3cea68d45e0b4f1f
digpolo <digpolo@gmail.com>
2023-10-03 23:43:30 -0500

    CU-85zu2x4ah - header

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -4,8 +4,8 @@ import logo from '../../public/logo.svg'
 
 const NavBar = () => {
   return (
-    <div className='flex justify-center items-center py-10'>
-        <Image alt='logo hernan jojoa' src={logo}/>
+    <div className='flex justify-center items-center pb-8 pt-10 lg:p-2 '>
+        <Image alt='logo hernan jojoa' src={logo} width={300} height={300}/>
     </div>
   )
 }
