1a7bb534686af0af8f214b3789c18cc7f6dfef6e
digpolo <digpolo@gmail.com>
2023-11-13 10:35:16 -0500

    fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    output: 'export',
-    images: {
-      unoptimized: true,
-    },
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 }
-  
-
 module.exports = nextConfig
